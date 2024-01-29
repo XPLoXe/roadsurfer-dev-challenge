f31227b6b722346f41ac1ce1104e0596b089a54f
Alejandro Bocchicchio <aleb2800@gmail.com>
2024-01-29 21:28:50 +0100

    Autocomplete.vue logic

--- src/components/Autocomplete.vue
+++ src/components/Autocomplete.vue
@@ -1,7 +1,57 @@
 <template>
-  <div></div>
+  <div>
+    <input
+      type="text"
+      v-model="searchTerm"
+      @input="onSearch"
+      placeholder="Search stations..."
+      class=""
+    />
+    <ul v-if="showSuggestions && suggestions.length" class="cursor-pointer">
+      <li
+        v-for="suggestion in suggestions"
+        :key="suggestion.id"
+        @click="selectStation(suggestion)"
+        class="pointer hover:bg-gray-200"
+      >
+        {{ suggestion.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
 
-<style lang="scss" scoped></style>
+const searchTerm = ref("");
+const suggestions = ref([]);
+const showSuggestions = ref(false);
+const emit = defineEmits(["station-selected"]);
+
+const onSearch = async () => {
+  if (searchTerm.value) {
+    try {
+      const response = await axios.get(
+        `https://605c94c36d85de00170da8b4.mockapi.io/stations`
+      );
+      suggestions.value = response.data.filter((station) =>
+        station.name.toLowerCase().includes(searchTerm.value.toLowerCase())
+      );
+      showSuggestions.value = true;
+    } catch (error) {
+      console.error("Error fetching stations:", error);
+    }
+  } else {
+    suggestions.value = [];
+    showSuggestions.value = false;
+  }
+};
+
+const selectStation = (station) => {
+  searchTerm.value = station.name;
+  showSuggestions.value = false;
+  // Emit an event with the selected station
+  emit("station-selected", station);
+};
+</script>
