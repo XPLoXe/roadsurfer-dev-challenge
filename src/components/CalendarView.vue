26bb1a7229f45e11b17d1d9224d6ef38daa4bf00
Alejandro Bocchicchio <aleb2800@gmail.com>
2024-01-29 16:59:35 +0100

    Calendar Added

--- src/components/CalendarView.vue
+++ src/components/CalendarView.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    <h2 class="text-3xl text-red-600 shadow-lg shadow-black">
-      This is the Calendar View
-    </h2>
+  <div class="flex flex-col items-center justify-center min-h-screen space-y-6">
+    <div class="items-center px-6 py-2 bg-cyan-200 rounded-xl">
+      <h2 class="text-xl font-bold">Calendar</h2>
+    </div>
+    <div>
+      <VCalendar />
+    </div>
     <RouterLink to="/booking/1">Booking 1</RouterLink>
   </div>
 </template>
