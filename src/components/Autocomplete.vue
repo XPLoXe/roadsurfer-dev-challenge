536e2e8eba937729919915297162495c104373c1
Alejandro Bocchicchio <aleb2800@gmail.com>
2024-01-30 19:41:17 +0100

    Autocomplete.vue is now reusable and generic

--- src/components/Autocomplete.vue
+++ src/components/Autocomplete.vue
@@ -4,18 +4,18 @@
       type="text"
       v-model="searchTerm"
       @input="onSearch"
-      placeholder="Search stations..."
+      :placeholder="placeholderText"
       class="w-full px-10 py-3 rounded-full focus:outline-none"
     />
     <ul
       v-if="showSuggestions && suggestions.length"
-      class="fixed z-20 px-6 py-3 m-2 overflow-auto bg-orange-400 rounded-lg shadow-lg cursor-pointer min-h-10 max-w-36"
+      class="autocomplete-suggestions"
     >
       <li
         v-for="suggestion in suggestions"
         :key="suggestion.id"
-        @click="selectStation(suggestion)"
-        class="px-2 py-1 mx-auto font-bold transition-all duration-300 pointer hover:scale-110 rounded-xl"
+        @click="selectSuggestion(suggestion)"
+        class="autocomplete-suggestion"
       >
         {{ suggestion.name }}
       </li>
@@ -24,26 +24,34 @@
 </template>
 
 <script setup>
-import { ref, defineEmits } from "vue";
+import { ref, defineEmits, watch, toRefs, defineProps } from "vue";
 import axios from "axios";
 
+const props = defineProps({
+  apiUrl: String,
+  placeholderText: String,
+});
+
+const emit = defineEmits(["selection"]);
 const searchTerm = ref("");
 const suggestions = ref([]);
 const showSuggestions = ref(false);
-const emit = defineEmits(["station-selected"]);
 
 const onSearch = async () => {
   if (searchTerm.value) {
     try {
-      const response = await axios.get(
-        `https://605c94c36d85de00170da8b4.mockapi.io/stations`
-      );
+      const response = await axios.get(`${props.apiUrl}`);
       suggestions.value = response.data.filter((station) =>
         station.name.toLowerCase().includes(searchTerm.value.toLowerCase())
       );
       showSuggestions.value = true;
+      // const response = await axios.get(
+      //   `https://605c94c36d85de00170da8b4.mockapi.io/stations`
+      // );
+      // suggestions.value = response.data.filter((station) =>
+      //   station.name.toLowerCase().includes(searchTerm.value.toLowerCase())
     } catch (error) {
-      console.error("Error fetching stations:", error);
+      console.error("Error fetching data:", error);
     }
   } else {
     suggestions.value = [];
@@ -51,11 +59,9 @@ const onSearch = async () => {
   }
 };
 
-const selectStation = (station) => {
-  searchTerm.value = station.name;
-  showSuggestions.value = false;
-  // Emit an event with the selected station
-  emit("station-selected", station);
+const selectSuggestion = (suggestion) => {
   searchTerm.value = "";
+  showSuggestions.value = false;
+  emit("selection", suggestion);
 };
 </script>
