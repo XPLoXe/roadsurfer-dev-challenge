ebee25afd6d3978f6ef53ff4a254d35f42fe6585
Alejandro Bocchicchio <aleb2800@gmail.com>
2024-01-30 11:52:37 +0100

    Dragging customer from BookingList.vue

--- src/components/BookingList.vue
+++ src/components/BookingList.vue
@@ -4,7 +4,10 @@
       <li
         v-for="booking in filteredBookings"
         :key="booking.id"
-        class="hover:text-blue-800 hover:underline"
+        class="flex flex-row items-center space-x-2 hover:text-blue-800 hover:underline"
+        :draggable="true"
+        @dragstart="handleDragStart(index, booking)"
+        @dragover.prevent
       >
         <RouterLink :to="getBookingDetailLink(booking)">
           {{ booking.customerName }} - {{ formatDate(booking.startDate) }} to
@@ -12,11 +15,30 @@
         </RouterLink>
       </li>
     </ul>
+    <div class="flex flex-row justify-center mt-4" v-show="isDragging">
+      <img
+        src="../images/fa-pen.svg"
+        alt=""
+        class="w-8 h-8 p-2 rounded-full ficon"
+        :class="{ 'drag-hover': draggingOverEdit }"
+        @dragover.prevent
+        @dragenter="draggingOverEdit = true"
+        @dragleave="draggingOverEdit = false"
+        @drop="handleEditDrop"
+      />
+      <img
+        src="../images/fa-trash.svg"
+        alt=""
+        class="w-8 h-8 p-2 rounded-full ficon"
+        @click="deleteBooking(draggedItem.booking)"
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { defineProps } from "vue";
+import { defineProps, ref, onMounted, onUnmounted } from "vue";
+import { useRouter } from "vue-router";
 
 const props = defineProps({
   filteredBookings: Array,
@@ -30,5 +52,45 @@ const formatDate = (selectedDate) => {
 const getBookingDetailLink = (booking) => {
   const bookingParam = encodeURIComponent(JSON.stringify(booking));
   return `/booking/${booking.id}?data=${bookingParam}`;
+};
+
+//Drag logic
+const router = useRouter();
+const isDragging = ref(false);
+const draggedItem = ref(null);
+const draggingOverEdit = ref(false);
+
+const handleDragStart = (index, booking) => {
+  isDragging.value = true;
+  draggedItem.value = booking;
+};
+
+const handleEditDrop = () => {
+  if (draggingOverEdit.value && draggedItem.value) {
+    router.push(getBookingDetailLink(draggedItem.value));
+  }
+  isDragging.value = false;
+  draggingOverEdit.value = false;
+};
+
+const resetDragState = () => {
+  isDragging.value = false;
+};
+
+onMounted(() => {
+  window.addEventListener("dragend", resetDragState);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("dragend", resetDragState);
+});
+
+//Handle edit and delete
+const deleteBooking = (booking) => {
+  console.log(`Deleting booking ${booking.id}`);
+};
+
+const editBooking = (booking) => {
+  console.log(`Editing booking ${booking.id}`);
 };
 </script>
