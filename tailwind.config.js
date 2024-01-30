40cba6c2913b82641cf74022149223e2029fc5b3
Alejandro Bocchicchio <aleb2800@gmail.com>
2024-01-30 15:22:21 +0100

    Adding some background images for testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ export default {
       fontFamily: {
         sans: ["Bai Jamjuree", "sans-serif"],
       },
+      backgroundImage: {
+        "main-background": "url('/src/images/bg-image2.jpg')",
+        "desktop-background": "url('/src/images/bg-desktop.jpg')",
+      },
     },
   },
   plugins: [],
