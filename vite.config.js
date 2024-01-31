21758cdfd8db402a02a8870dda4eba515c0a703e
Alejandro Bocchicchio <aleb2800@gmail.com>
2024-01-31 10:29:40 +0100

    Preparing for deploying in GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), ghPages()],
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/<roadsurfer-dev-challenge>/"
+      : "/",
+});
